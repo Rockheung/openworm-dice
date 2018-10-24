9b6ecaeb521d47c22e527e0071613b404f492dd3
Rockheung <rockheung@gmail.com>
2018-10-24 18:21:55 +0900

    make .pow/worm.db

--- run.py
+++ run.py
@@ -1,15 +1,11 @@
+import os
 import PyOpenWorm as P
 from PyOpenWorm.context import Context
 from PyOpenWorm.worm import Worm
 
-readme_conf = \
-    {
-        "rdf.source" : "ZODB",
-        "rdf.store_conf" : ".pow/worm.db"
-    }
 
 if __name__ == '__main__':
-    P.connect(str(readme_conf))
+    P.connect(os.path.join('PyOpenWorm', 'readme.conf'))
     ctx = Context(ident="http://openworm.org/entities/bio#worm0-data")
 
     #net = ctx.stored(Worm)().neuron_network()
